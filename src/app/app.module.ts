3380259706d96b005e06ee009a8aaf13349fff8c
lalit-optiliza <lalit@optiliza.com>
2018-03-13 15:49:18 +0530

    Loading custom tag inserted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { CustomComponent } from './custom/custom.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    CustomComponent
   ],
   imports: [
     BrowserModule,
